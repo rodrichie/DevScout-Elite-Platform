38dc2c8e0c7e4a72011b9dba10ccd657afc5c65e
rodrichie <nabasarodrick@gmail.com>
2025-11-20 18:08:21 +0300

    Add semantic and auth routers to API routers module

--- api/routers/__init__.py
+++ api/routers/__init__.py
@@ -1,6 +1,6 @@
 """
 API Routers Module
 """
-from . import candidates, skills, github, analytics
+from . import candidates, skills, github, analytics, semantic, auth
 
-__all__ = ['candidates', 'skills', 'github', 'analytics']
+__all__ = ['candidates', 'skills', 'github', 'analytics', 'semantic', 'auth']
